5f6902450d06af27e0959c9267be2fae7dbfc4c9
Piotr Bernatowicz <piotrbernatowicz@tutanota.com>
2018-06-26 13:34:32 +0200

    Change function for enter key in text field

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@
 	document.getElementById('country-name').addEventListener('keyup', function(event){
 		event.preventDefault();
 		if (event.keyCode === 13) {
-			document.getElementById('search').click();
+			searchCountries();
 		}
 	});
 
